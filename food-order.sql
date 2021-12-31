7b65cd73295efe67f124bf5a6fc9ceb1e3b58a87
taijul-mohim <59666569+taijul-mohim@users.noreply.github.com>
2021-12-31 11:38:57 +0600

    Add files via upload

--- food-order.sql
+++ food-order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2021 at 02:43 PM
+-- Generation Time: Dec 30, 2021 at 01:25 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
